dcfe1fe43b5d4d50d13abe7c9e5607e8da64f4c7
Mark Seliverstov <smdmrr00@gmail.com>
2024-11-09 00:01:45 +0100

    chore: add transcription with openai

--- src/tfm/__main__.py
+++ src/tfm/__main__.py
@@ -1,24 +1,27 @@
 import asyncio
-from os import environ, getenv
+from os import getenv
+from typing import BinaryIO
 
-from aiogram import Bot, Dispatcher, html
+from aiogram import Bot, Dispatcher, F, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from openai import OpenAI
+from structlog import BoundLogger, get_logger
 
-for k, v in environ.items():
-    print(f"{k}: {v}")
+logger: BoundLogger = get_logger()
 
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = getenv("BOT_TOKEN")
 
 # All handlers should be attached to the Router (or Dispatcher)
-
+openai = OpenAI(api_key=getenv("OPENAI_API_KEY"))
 dp = Dispatcher()
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
 
-@dp.message(CommandStart())
+@dp.message_handler(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """
     This handler receives messages with `/start` command
@@ -31,25 +34,30 @@ async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
 
-@dp.message()
+@dp.message(F.voice)
 async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
     try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
+        assert message.voice
+        logger.info(
+            f"Downloading voice with {message.voice.file_id=} and {message.voice.mime_type=}"
+        )
+        voice_file: BinaryIO | None = await bot.download(message.voice)
+        if not voice_file:
+            await message.answer("Failed to download voice")
+            return
+        logger.info(f"Downloaded voice with {len(voice_file.read())=}")
+        transcription = openai.audio.transcriptions.create(
+            file=voice_file, model="whisper-1", response_format="text"
+        )
+        await message.answer(transcription)
+
     except TypeError:
-        # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
 
 
 async def start():
     # Initialize Bot instance with default bot properties which will be passed to all API calls
     print(TOKEN)
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
     # And the run events dispatching
     await dp.start_polling(bot)
