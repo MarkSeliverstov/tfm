d9afd9f76533fa5fc9a0f0b786af42dfa94e884f
Mark Seliverstov <smdmrr00@gmail.com>
2024-11-11 22:25:02 +0100

    feat: using postgre instead of google sheets
    Simplify UX for average user :D

--- src/tfm/database.py
+++ src/tfm/database.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from importlib import resources
 from typing import Any
 
@@ -33,6 +34,45 @@ class PostgresDatabase:
         con: asyncpg.Connection
         async with self.pg_pool.acquire() as con:
             await con.execute(query, id, initial_balance, initial_balance, [])
+
+    async def change_transactions_types(self, user_id: int, types: list[str]) -> None:
+        assert self.pg_pool
+        query: str = """
+        UPDATE users
+        SET transactions_types = $2
+        WHERE id = $1
+        """
+        con: asyncpg.Connection
+        async with self.pg_pool.acquire() as con:
+            await con.execute(query, user_id, types)
+
+    async def get_transactions_types(self, user_id: int) -> list[str]:
+        assert self.pg_pool
+        query: str = """
+        SELECT transactions_types FROM users WHERE id = $1
+        """
+        con: asyncpg.Connection
+        async with self.pg_pool.acquire() as con:
+            return await con.fetchval(query, user_id)  # type: ignore
+
+    async def add_transaction(self, user_id: int, amount: Decimal, transaction_type: str) -> None:
+        assert self.pg_pool
+        update_current_balance_query: str = """
+        UPDATE users
+        SET current_balance = current_balance + $1
+        WHERE id = $2
+        """
+        insert_transaction_query: str = """
+        INSERT INTO transactions (user_id, amount, type) VALUES ($1, $2, $3)
+        """
+        con: asyncpg.Connection
+        async with self.pg_pool.acquire() as con:
+            async with con.transaction():
+                existed_types: list[str] = await self.get_transactions_types(user_id)
+                if transaction_type not in existed_types:
+                    raise ValueError(f"Transaction type {transaction_type} is not valid")
+                await con.execute(update_current_balance_query, amount, user_id)
+                await con.execute(insert_transaction_query, user_id, amount, transaction_type)
 
     async def get_user(self, id: int) -> dict[str, Any]:
         assert self.pg_pool
