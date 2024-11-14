c61ce3a253d83c458a194fd06e03692a7f4eaf4f
Mark Seliverstov <smdmrr00@gmail.com>
2024-11-14 19:21:55 +0100

    chore: remove unused config

--- src/tfm/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class Config:
-    BOT_TOKEN: str = os.environ["BOT_TOKEN"]
-    OPENAI_API_KEY: str = os.environ["OPENAI_API_KEY"]
-    MY_TELEGRAM_ID: int = int(os.environ["MY_TELEGRAM_ID"])
-    DATABASE_DSN: str = os.environ["DATABASE_DSN"]
